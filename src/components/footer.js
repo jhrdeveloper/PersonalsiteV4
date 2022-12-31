a171accb8d7253f0bfdf7fe74e2a62f774916e1c
Julio Honorio <julio-developer@hotmail.com>
2022-12-30 20:46:14 -0500

    Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/jhrdeveloper/PersonalsiteV4">
+          <div>Diseñado &amp; Creado por Julio Honorio</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
