49369982ccae4a804b5d18a01b90fd95bb12c8e4
Julio Honorio <julio-developer@hotmail.com>
2022-12-30 22:00:22 -0500

    New changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Julio Honorio, estudiante de ingeniería de software, especializado en construir (y ocasionalmente diseñar) experiencias digitales.',
     siteUrl: 'https://juliodeveloper.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: 'static/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@jhrdeveloper',
   },
   plugins: [
