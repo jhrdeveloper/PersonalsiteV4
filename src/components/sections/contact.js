205cdf0b87a6c958a0856fcfc3fa9cebaf84b77d
Julio Honorio <julio-developer@hotmail.com>
2022-12-30 20:43:17 -0500

    Section contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -55,17 +55,16 @@ const Contact = () => {
 
   return (
     <StyledContactSection id="contact" ref={revealContainer}>
-      <h2 className="numbered-heading overline">What’s Next?</h2>
+      <h2 className="numbered-heading overline">¿Qué sigue?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">Ponte en contacto</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Aunque actualmente estoy buscando nuevas oportunidades, mi bandeja de entrada siempre está disponible.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        ¡Salúdame!
       </a>
     </StyledContactSection>
   );
